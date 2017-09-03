3335e3be0274c983d4d5029a744ff58c15d7d5fe
viyanateaa <viyanateaa@gmail.com>
2017-09-03 23:11:23 +0200

    added login button, register button to xml and java file

--- app/src/main/java/kidsinnature/tensaii/com/kidinnature/Login.java
+++ app/src/main/java/kidsinnature/tensaii/com/kidinnature/Login.java
@@ -2,12 +2,35 @@ package kidsinnature.tensaii.com.kidinnature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    Button btLogin;
+    EditText etUserName, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUserName =(EditText) findViewById(R.id.etUserName);
+        etPassword= (EditText) findViewById(R.id.etPassword);
+        btLogin =(Button) findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btLogin:
+
+                break;
+        }
     }
 }
--- app/src/main/java/kidsinnature/tensaii/com/kidinnature/Register.java
+++ app/src/main/java/kidsinnature/tensaii/com/kidinnature/Register.java
@@ -2,12 +2,39 @@ package kidsinnature.tensaii.com.kidinnature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    Button btRegister;
+    EditText etName,etUserName, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName= (EditText) findViewById(R.id.etName);
+        etUserName= (EditText)findViewById(R.id.etUserName);
+        etPassword=(EditText)findViewById(R.id.etPassword);
+
+        btRegister =(Button)findViewById(R.id.btRegister);
+
+        btRegister.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btRegister:
+
+
+
+                break;
+        }
     }
 }
